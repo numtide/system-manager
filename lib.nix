fe6fb907f7edfefea7484ecfc4d20caf1d7d67f2
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-02-01 17:36:07 +0000

    Add an activation script.

--- lib.nix
+++ lib.nix
@@ -5,6 +5,8 @@ in
 {
   makeServiceConfig = { system, module }:
     let
+      pkgs = nixpkgs.legacyPackages.${system};
+
       nixosConfig = lib.nixosSystem {
         inherit system;
         specialArgs = { };
@@ -14,10 +16,19 @@ in
         (serviceName:
           nixosConfig.config.systemd.units."${serviceName}.service".unit)
         nixosConfig.config.service-manager.services;
+
+      servicesPath =
+        pkgs.writeTextFile {
+          name = "services";
+          destination = "/services.json";
+          text = lib.generators.toJSON { } services;
+        };
+      activationScript = pkgs.writeShellScript "activate" ''
+        echo "${servicesPath}"
+      '';
     in
-    nixpkgs.legacyPackages.${system}.writeTextFile {
-      name = "services";
-      destination = "/services.json";
-      text = lib.generators.toJSON { } services;
-    };
+    pkgs.linkFarmFromDrvs "service-manager" [
+      servicesPath
+      activationScript
+    ];
 }
