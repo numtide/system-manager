95bd335d8c47c326fa3d95cea586273cf8473b16
R-VdP <141248+R-VdP@users.noreply.github.com>
2023-02-01 16:47:43 +0000

    Move the function to generate the store path into lib.

--- flake.nix
+++ flake.nix
@@ -6,28 +6,32 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }: {
-    serviceConfig =
-      let
-        system = flake-utils.lib.system.x86_64-linux;
-        lib = nixpkgs.lib;
-        nixosConfig = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { };
-          modules = [ ./modules ];
+    serviceConfig = self.lib.makeServiceConfig
+      [
+        "service-1"
+        "service-2"
+      ];
+
+    lib = {
+      makeServiceConfig = serviceNames:
+        let
+          system = flake-utils.lib.system.x86_64-linux;
+          lib = nixpkgs.lib;
+          nixosConfig = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = { };
+            modules = [ ./modules ];
+          };
+          services = lib.flip lib.genAttrs
+            (serviceName:
+              nixosConfig.config.systemd.units."${serviceName}.service".unit)
+            serviceNames;
+        in
+        nixpkgs.legacyPackages.${system}.writeTextFile {
+          name = "services";
+          destination = "/services.json";
+          text = lib.generators.toJSON { } services;
         };
-        services = lib.flip lib.genAttrs
-          (serviceName:
-            nixosConfig.config.systemd.units."${serviceName}.service".unit)
-          # TODO: generate this list from the config instead of hard coding
-          [
-            "service-1"
-            "service-2"
-          ];
-      in
-      nixpkgs.legacyPackages.${system}.writeTextFile {
-        name = "services";
-        destination = "/services.json";
-        text = lib.generators.toJSON { } services;
-      };
+    };
   };
 }
