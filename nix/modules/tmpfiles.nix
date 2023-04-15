f9ce66b836180c67a43e353294d8efea5fe7ca3f
Aaron Andersen <aaron@fosslib.net>
2023-04-15 16:55:46 +0000

    Provide an implementation of the systemd.tmpfiles.rules NixOS option

---
+++ nix/modules/tmpfiles.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+let
+  inherit (lib) types;
+in
+{
+  options = {
+    systemd.tmpfiles.rules = lib.mkOption {
+      type = types.listOf types.str;
+      default = [];
+      example = [ "d /tmp 1777 root root 10d" ];
+      description = lib.mdDoc ''
+        Rules for creation, deletion and cleaning of volatile and temporary files
+        automatically. See
+        {manpage}`tmpfiles.d(5)`
+        for the exact format.
+      '';
+    };
+  };
+
+  config = {
+    environment.etc."tmpfiles.d/00-system-manager.conf".text = ''
+      # This file is created automatically and should not be modified.
+      # Please change the option ‘systemd.tmpfiles.rules’ instead.
+      ${lib.concatStringsSep "\n" config.systemd.tmpfiles.rules}
+    '';
+  };
+}
