af437440061b878c9b8a69037383e98d240b1619
Ramses <ramses@well-founded.dev>
2023-04-27 14:57:00 +0200

    Add Aarch64 tests and include multiple test images. (#9)

---
+++ examples/example.nix
@@ -0,0 +1,75 @@
+{ lib, pkgs, ... }: {
+  config = {
+    nixpkgs.hostPlatform = "x86_64-linux";
+
+    services.nginx.enable = true;
+
+    environment.etc = {
+      foo = {
+        text = ''
+          This is just a test!
+        '';
+        target = "foo_test";
+      };
+
+      "foo.conf".text = ''
+        launch_the_rockets = true
+      '';
+
+      "baz/bar/foo2" = {
+        text = ''
+          Another test!
+        '';
+        mode = "symlink";
+      };
+
+      foo3 = {
+        text = "boo!";
+        mode = "0700";
+        user = "root";
+        group = "root";
+      };
+
+      "a/nested/example/foo3" = {
+        text = "boo!";
+        mode = "0764";
+        user = "root";
+        group = "root";
+      };
+
+      "a/nested/example2/foo3" = {
+        text = "boo!";
+        mode = "0764";
+        user = "root";
+        group = "root";
+      };
+
+      out-of-store = {
+        source = "/run/systemd/system/";
+      };
+    };
+    systemd.services =
+      lib.listToAttrs
+        (lib.flip lib.genList 10 (ix:
+          lib.nameValuePair "service-${toString ix}"
+            {
+              enable = true;
+              description = "service-${toString ix}";
+              wants = [ "network-online.target" ];
+              after = [
+                "network-online.target"
+              ];
+              serviceConfig = {
+                Type = "oneshot";
+                RemainAfterExit = true;
+                ExecReload = "${lib.getBin pkgs.coreutils}/bin/true";
+              };
+              wantedBy = [ "system-manager.target" ];
+              requiredBy = lib.mkIf (ix > 5) [ "service-0.service" ];
+              script = ''
+                sleep ${if ix > 5 then "2" else "1"}
+              '';
+            })
+        );
+  };
+}
