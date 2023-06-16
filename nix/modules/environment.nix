7ab72d8c7f3d43e2855a1eeb0087d54f5b79f251
r-vdp <ramses@well-founded.dev>
2023-06-17 00:42:19 +0200

    Add a pathsToLink option.

--- nix/modules/environment.nix
+++ nix/modules/environment.nix
@@ -6,6 +6,11 @@
       type = lib.types.listOf lib.types.package;
       default = [ ];
     };
+
+    pathsToLink = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
   };
 
   config =
@@ -13,22 +18,28 @@
       pathDir = "/run/system-manager/sw";
     in
     {
-      environment.etc = {
-        "profile.d/system-manager-path.sh".source =
-          pkgs.writeText "system-manager-path.sh" ''
-            export PATH=${pathDir}/bin/:''${PATH}
-          '';
+      environment = {
+        pathsToLink = [
+          "/bin"
+        ];
 
-        # TODO: figure out how to properly add fish support. We could start by
-        # looking at what NixOS and HM do to set up the fish env.
-        #"fish/conf.d/system-manager-path.fish".source =
-        #  pkgs.writeTextFile {
-        #    name = "system-manager-path.fish";
-        #    executable = true;
-        #    text = ''
-        #      set -gx PATH "${pathDir}/bin/" $PATH
-        #    '';
-        #  };
+        etc = {
+          "profile.d/system-manager-path.sh".source =
+            pkgs.writeText "system-manager-path.sh" ''
+              export PATH=${pathDir}/bin/:''${PATH}
+            '';
+
+          # TODO: figure out how to properly add fish support. We could start by
+          # looking at what NixOS and HM do to set up the fish env.
+          #"fish/conf.d/system-manager-path.fish".source =
+          #  pkgs.writeTextFile {
+          #    name = "system-manager-path.fish";
+          #    executable = true;
+          #    text = ''
+          #      set -gx PATH "${pathDir}/bin/" $PATH
+          #    '';
+          #  };
+        };
       };
 
       systemd.services.system-manager-path = {
@@ -44,7 +55,7 @@
             pathDrv = pkgs.buildEnv {
               name = "system-manager-path";
               paths = config.environment.systemPackages;
-              pathsToLink = [ "/bin" ];
+              inherit (config.environment) pathsToLink;
             };
           in
           ''
