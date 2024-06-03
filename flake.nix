44d291700b9c7cd64f8bc0fdb6a0dbffa0a26d22
r-vdp <ramses@well-founded.dev>
2024-06-03 11:28:15 +0200

    Remove unused follows

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
       };
     };
     rust-overlay = {
