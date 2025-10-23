bb981aefe114f7fe671b0f2f42bc5e3de2bff95c
Jean-François Roche <jfroche@pyxel.be>
2025-10-23 10:07:07 +0200

    Move tests into a separate sub flake (#272)
    Prevent dev inputs in the root flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+    }:
     let
       systems = [
         "aarch64-linux"
@@ -19,17 +22,6 @@
             pkgs = nixpkgs.legacyPackages.${system};
           }
         );
-      nix-vm-test-lib =
-        let
-          rev = "e34870b8dd2c2d203c05b4f931b8c33eaaf43b81";
-          sha256 = "sha256:1qp1fq96kv9i1nj20m25057pfcs1b1c9bj4502xy7gnw8caqr30d";
-        in
-        "${
-          builtins.fetchTarball {
-            url = "https://github.com/numtide/nix-vm-test/archive/${rev}.tar.gz";
-            inherit sha256;
-          }
-        }/lib.nix";
     in
     {
       lib = import ./nix/lib.nix { inherit nixpkgs; };
@@ -47,11 +39,6 @@
         };
       };
 
-      # Only useful for quick tests
-      systemConfigs.default = self.lib.makeSystemConfig {
-        modules = [ ./examples/example.nix ];
-      };
-
       formatter = eachSystem ({ pkgs, ... }: pkgs.treefmt);
 
       devShells = eachSystem (
@@ -61,29 +48,11 @@
         }
       );
 
-      checks = (
-        nixpkgs.lib.recursiveUpdate
-          (eachSystem (
-            { system, ... }:
-            {
-              system-manager = self.packages.${system}.default;
-            }
-          ))
-          {
-            x86_64-linux =
-              let
-                system = "x86_64-linux";
-              in
-              (import ./test/nix/modules {
-                inherit system;
-                inherit (nixpkgs) lib;
-                nix-vm-test = import nix-vm-test-lib {
-                  inherit nixpkgs;
-                  inherit system;
-                };
-                system-manager = self;
-              });
-          }
+      checks = eachSystem (
+        { system, ... }:
+        {
+          system-manager = self.packages.${system}.default;
+        }
       );
     };
 }
