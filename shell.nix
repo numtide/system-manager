516ca11ad1441885c2725e4233e43ed9c2d426f3
Jean-François Roche <jfroche@pyxel.be>
2026-01-19 23:32:35 +0100

    fix(shell): add darwin compatibility
    - Remove unnecessary systemdMinimal dependency (dbus crate only needs libdbus)
    - Add required libiconv for darwin linking

--- shell.nix
+++ shell.nix
@@ -7,26 +7,16 @@ in
 pkgs.mkShellNoCC {
   shellHook = ''
     ${pkgs.pre-commit}/bin/pre-commit install --install-hooks --overwrite
-    export PKG_CONFIG_PATH="${
-      pkgs.lib.makeSearchPath "lib/pkgconfig" [
-        pkgs.dbus.dev
-        pkgs.systemdMinimal.dev
-      ]
-    }"
+    export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPath "lib/pkgconfig" [ pkgs.dbus.dev ]}"
     export LIBCLANG_PATH="${llvm.libclang}/lib"
     # for rust-analyzer
     export RUST_SRC_PATH="${pkgs.rustPlatform.rustLibSrc}"
     export RUST_BACKTRACE=1
-    export RUSTFLAGS="${
-      pkgs.lib.concatStringsSep " " [
-        "-L${pkgs.lib.getLib pkgs.systemdMinimal}/lib"
-        "-lsystemd"
-      ]
-    }"
   '';
-  buildInputs = with pkgs; [
-    dbus
-  ];
+  buildInputs = [
+    pkgs.dbus
+  ]
+  ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ pkgs.libiconv ];
   nativeBuildInputs = with pkgs; [
     llvm.clang
     pkg-config
