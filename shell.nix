f8b065e64a0cb3b2fc714f14b91b6749a97e6069
Jean-François Roche <jfroche@pyxel.be>
2026-03-10 14:11:05 +0100

    fix: use flake-compat for default.nix and shell.nix
    nix-build was failing because default.nix imported nix/lib.nix without
    passing the required userborn argument. 
    
    We replace both default.nix and shell.nix with flake-compat shims.

--- shell.nix
+++ shell.nix
@@ -1,38 +1,11 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-let
-  llvm = pkgs.llvmPackages_latest;
-in
-pkgs.mkShellNoCC {
-  shellHook = ''
-    ${pkgs.pre-commit}/bin/pre-commit install --install-hooks --overwrite
-    export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPath "lib/pkgconfig" [ pkgs.dbus.dev ]}"
-    export LIBCLANG_PATH="${llvm.libclang}/lib"
-    # for rust-analyzer
-    export RUST_SRC_PATH="${pkgs.rustPlatform.rustLibSrc}"
-    export RUST_BACKTRACE=1
-  '';
-  buildInputs = [
-    pkgs.dbus
-  ]
-  ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ pkgs.libiconv ];
-  nativeBuildInputs = with pkgs; [
-    llvm.clang
-    pkg-config
-    rustc
-    cargo
-    # Formatting
-    pre-commit
-    treefmt
-    nixfmt
-    rustfmt
-    clippy
-    mdbook
-    mdformat
-    rust-analyzer
-    gh
-    # Testing tools
-    parallel
-  ];
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url =
+      lock.nodes.flake-compat.locked.url
+        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) { src = ./.; }).shellNix
