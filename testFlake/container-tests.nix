c9cdfb94b6875eb455d79c765cbe69bbe13a63ed
picnoir <picnoir@alternativebit.fr>
2026-03-19 11:43:53 +0100

    Openssh: add user authorizedkey test
    This test made me realize we might have a problem: we're not
    supporting the NixOS restartTriggers trick to force units restart
    during activation when a etc file changed.
    
    We might have to implement that at some point.

--- testFlake/container-tests.nix
+++ testFlake/container-tests.nix
@@ -379,7 +379,7 @@ forEachDistro "example" {
 // forEachDistro "ssh" {
   modules = [
     (
-      { ... }:
+      { pkgs, ... }:
       {
         programs.ssh.enable = true;
         services.openssh.enable = true;
@@ -396,7 +396,27 @@ forEachDistro "example" {
             publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
           };
         };
+
+        # Meh. Not great, but that's the only way I found to share this with the container.
+        environment.etc."privatekey" = {
+          source = pkgs.writeText "private-key" ''
+            -----BEGIN OPENSSH PRIVATE KEY-----
+            b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+            QyNTUxOQAAACDTaOxU7gQHrj8hPQks0u4tiVmRhF1oBAl5+2EkQ9fYBAAAAJh3B3i6dwd4
+            ugAAAAtzc2gtZWQyNTUxOQAAACDTaOxU7gQHrj8hPQks0u4tiVmRhF1oBAl5+2EkQ9fYBA
+            AAAEBc2BENaT8wrgOp3DsEbvS2Lt0NeTrfVztH9NLLPIE1r9No7FTuBAeuPyE9CSzS7i2J
+            WZGEXWgECXn7YSRD19gEAAAAD3BpY25vaXJAYW5hcnJlcwECAwQFBg==
+            -----END OPENSSH PRIVATE KEY-----
+          '';
+          mode = "600";
+          user = "root";
+          group = "root";
+        };
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNo7FTuBAeuPyE9CSzS7i2JWZGEXWgECXn7YSRD19gE"
+        ];
       }
+
     )
   ];
   testScriptFunction =
@@ -438,13 +458,19 @@ forEachDistro "example" {
           assert "/etc/ssh/ssh_known_hosts" in config_content, \
               f"Expected /etc/ssh/ssh_known_hosts path in ssh_config, got: {config_content}"
 
-      with subtest("ssh server"):
-          machine.wait_for_unit("sshd.service")
+      with subtest("ssh server test"):
+          # We have to manually restart the ssh service.
+          # The restartTriggers mechanism is not implemented in the system-manager
+          # activation :(
+          machine.systemctl("restart ssh.service")
+          machine.wait_for_unit("ssh.service")
           sshd_config = machine.file("/etc/ssh/sshd_config")
           assert sshd_config.exists, "/etc/ssh/sshd_config should exist"
           sshd_content = machine.succeed("cat /etc/ssh/sshd_config")
           assert "Subsystem sftp /nix/store/" in sshd_content, \
               "/etc/ssh/sshd_config does not appear to be the system-manager provided one."
+          machine.succeed('ssh -i /etc/privatekey -o "StrictHostKeyChecking no" root@localhost echo ok')
+
 
       with subtest("deactivation removes known hosts file"):
           machine.succeed("${toplevel}/bin/deactivate")
