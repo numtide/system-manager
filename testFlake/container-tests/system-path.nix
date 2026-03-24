d9bbdc7e7abc155f2386babf0c913246270b1deb
Jean-François Roche <jfroche@pyxel.be>
2026-03-24 01:40:33 +0100

    feat: convert system-path test from VM to container

---
+++ testFlake/container-tests/system-path.nix
@@ -0,0 +1,122 @@
+{
+  forEachDistro,
+  system-manager,
+  system,
+  sops-nix,
+  ...
+}:
+
+let
+  newConfig = system-manager.lib.makeSystemConfig {
+    modules = [
+      (
+        { lib, pkgs, ... }:
+        {
+          imports = [ sops-nix.nixosModules.sops ];
+          config = {
+            nixpkgs.hostPlatform = system;
+
+            services.nginx.enable = false;
+
+            environment = {
+              etc = {
+                foo_new = {
+                  text = ''
+                    This is just a test!
+                  '';
+                };
+              };
+
+              systemPackages = [
+                pkgs.fish
+              ];
+            };
+
+            systemd.services = {
+              new-service = {
+                enable = true;
+                description = "new-service";
+                serviceConfig = {
+                  Type = "oneshot";
+                  RemainAfterExit = true;
+                  ExecReload = "${lib.getBin pkgs.coreutils}/bin/true";
+                };
+                wantedBy = [
+                  "system-manager.target"
+                  "default.target"
+                ];
+                script = ''
+                  sleep 2
+                '';
+              };
+            };
+
+            nix = {
+              enable = true;
+              settings = {
+                experimental-features = [
+                  "nix-command"
+                  "flakes"
+                ];
+                trusted-users = [ "zimbatm" ];
+              };
+            };
+
+            users.users.zimbatm = {
+              isNormalUser = true;
+              extraGroups = [
+                "wheel"
+                "sudo"
+              ];
+              initialPassword = "test123";
+            };
+
+            sops = {
+              age.generateKey = false;
+              age.keyFile = "/run/age-keys.txt";
+              defaultSopsFile = ../sops/secrets.yaml;
+              secrets.test = { };
+            };
+            systemd.services.sops-install-secrets = {
+              before = [ "sysinit-reactivation.target" ];
+              requiredBy = [ "sysinit-reactivation.target" ];
+            };
+          };
+        }
+      )
+    ];
+  };
+in
+
+forEachDistro "system-path" {
+  modules = [
+    ../../examples/example.nix
+  ];
+  extraPathsToRegister = [ newConfig ];
+  testScriptFunction =
+    { toplevel, hostPkgs, ... }:
+    ''
+      # Start the container
+      start_all()
+      machine.wait_for_unit("multi-user.target")
+      # System manager is trying to configure nix. Activation will get a partial error if we do not delete it.
+      machine.execute("rm -rf /etc/nix")
+
+      machine.fail("bash --login -c '$(which rg)'")
+      machine.fail("bash --login -c '$(which fd)'")
+
+      machine.activate()
+
+      machine.wait_for_unit("system-manager.target")
+      machine.wait_for_unit("system-manager-path.service")
+
+      machine.succeed("bash --login -c 'realpath $(which rg) | grep -F ${hostPkgs.ripgrep}/bin/rg'")
+      machine.succeed("bash --login -c 'realpath $(which fd) | grep -F ${hostPkgs.fd}/bin/fd'")
+
+      machine.activate("${newConfig}")
+
+      machine.fail("bash --login -c '$(which rg)'")
+      machine.fail("bash --login -c '$(which fd)'")
+      machine.succeed("bash --login -c 'realpath $(which fish) | grep -F ${hostPkgs.fish}/bin/fish'")
+    '';
+}
