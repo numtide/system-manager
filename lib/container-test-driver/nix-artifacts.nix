d6224cfb3740dd06e1a237f47d16b9613181930d
Jean-François Roche <jfroche@pyxel.be>
2026-03-31 18:59:31 +0200

    refactor: extract nix-artifacts data into JSON for automated updates

--- lib/container-test-driver/nix-artifacts.nix
+++ lib/container-test-driver/nix-artifacts.nix
@@ -2,19 +2,20 @@
   system,
 }:
 let
-  nixVersion = "2.33.0";
+  data = builtins.fromJSON (builtins.readFile ./nix-artifacts.json);
+  nixVersion = data.nixVersion;
 in
 {
   nix-installer =
     if system == "x86_64-linux" then
       builtins.fetchurl {
         url = "https://github.com/NixOS/nix-installer/releases/download/${nixVersion}/nix-installer-x86_64-linux";
-        sha256 = "sha256-+GTcBIJ56ulEaP/xja+oLajdGb+bHDka9WQkU4XIMNM=";
+        sha256 = data.nix-installer.x86_64-linux;
       }
     else if system == "aarch64-linux" then
       builtins.fetchurl {
         url = "https://github.com/NixOS/nix-installer/releases/download/${nixVersion}/nix-installer-aarch64-linux";
-        sha256 = "sha256-ociEB/P9kJAzUSxQCLmqOJEQpGuqvTQk+cEVtG6YIS4=";
+        sha256 = data.nix-installer.aarch64-linux;
       }
     else
       throw "Unsupported system: ${system}";
@@ -23,12 +24,12 @@ in
     if system == "x86_64-linux" then
       builtins.fetchurl {
         url = "https://releases.nixos.org/nix/nix-${nixVersion}/nix-${nixVersion}-x86_64-linux.tar.xz";
-        sha256 = "00cgpm2l3mcmxqwvsvak0qwd498x9azm588czb5p3brmcvin3bsl";
+        sha256 = data.nixTarball.x86_64-linux;
       }
     else if system == "aarch64-linux" then
       builtins.fetchurl {
         url = "https://releases.nixos.org/nix/nix-${nixVersion}/nix-${nixVersion}-aarch64-linux.tar.xz";
-        sha256 = "1v3z0qdfm6sa053qn39ijn2g9vsh1nrhykwsxx7piwlnvysn4hsw";
+        sha256 = data.nixTarball.aarch64-linux;
       }
     else
       throw "Unsupported system: ${system}";
