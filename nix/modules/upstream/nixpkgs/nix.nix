34f51ff8fe92b987d476651692ea6dd7a72f5322
Jean-François Roche <jfroche@pyxel.be>
2026-04-21 15:14:07 +0200

    feat: add nix package to systemPackages when nix.enable

--- nix/modules/upstream/nixpkgs/nix.nix
+++ nix/modules/upstream/nixpkgs/nix.nix
@@ -15,6 +15,7 @@
 
     (lib.mkIf config.nix.enable {
       environment.etc."nix/nix.conf".replaceExisting = true;
+      environment.systemPackages = [ config.nix.package ];
 
       # nix-flakes.nix always renders /etc/nix/registry.json, but we only want
       # to take over a pre-existing registry when we have something to put in
@@ -23,6 +24,7 @@
         enable = config.nix.registry != { };
         replaceExisting = true;
       };
+
       nix.settings.experimental-features = lib.mkDefault [
         "nix-command"
         "flakes"
